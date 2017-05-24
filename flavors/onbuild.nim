6f188dfa6f65fad67685b141db636656615fd5a3
Konstantin Molchanov <moigagoo@live.com>
2017-05-24 11:34:16 +0400

    Roll back replacement of build with install.

--- flavors/onbuild.nim
+++ flavors/onbuild.nim
@@ -10,9 +10,5 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 ONBUILD COPY . /usr/src/app
-#  if version >= "0.17.0":
-ONBUILD RUN nimble install -y
-#  else:
 ONBUILD RUN nimble build -y
-#  end if
 #end proc
