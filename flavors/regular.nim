f0720acc1c1b8a13330e7f13d8614965b938e2ab
Konstantin Molchanov <moigagoo@live.com>
2017-05-29 22:27:12 +0400

    Install Mercurial.

--- flavors/regular.nim
+++ flavors/regular.nim
@@ -6,7 +6,7 @@ FROM nimlang/nim:$version-ubuntu-slim
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apt-get update; apt-get install -y git
+RUN apt-get update; apt-get install -y git mercurial
 #  if version >= "0.16.0":
 RUN cd nim; nim c koch; ./koch tools; \
     ln -s `pwd`/bin/nimble /bin/nimble; \
@@ -26,7 +26,7 @@ FROM nimlang/nim:$version-alpine-slim
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apk add --no-cache git
+RUN apk add --no-cache git mercurial
 #  if version >= "0.16.0":
 RUN cd nim; nim c koch; ./koch tools; \
     ln -s `pwd`/bin/nimble /bin/nimble; \
