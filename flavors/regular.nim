38f78c270135647f51bffcdda45ae1d27b04497f
Guilherme Thomazi Bonicontro <guilherme.bonicontro@relayr.io>
2018-07-06 10:20:23 +0200

    reverting dumb renaming

--- flavors/regular.nim
+++ flavors/regular.nim
@@ -2,34 +2,39 @@
 #proc ubuntu*(version: string,
 #             labels: openarray[(string, string)] = {:}): string =
 #  result = ""
-FROM ubuntu
+FROM nimlang/nim:$version-ubuntu-slim
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apt-get update; apt-get install -y wget xz-utils g++
-RUN wget http://nim-lang.org/download/nim-${version}.tar.xz; \
-    tar xf nim-${version}.tar.xz; rm nim-${version}.tar.xz; \
-    mv nim-${version} nim; \
-    cd nim; sh build.sh; \
-    ln -s `pwd`/bin/nim /bin/nim
+RUN apt-get update; apt-get install -y git mercurial
+#  if version >= "0.16.0":
+RUN cd nim; nim c koch; ./koch tools; \
+    ln -s `pwd`/bin/nimble /bin/nimble; \
+    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest
+#  else:
+RUN git clone https://github.com/nim-lang/nimble.git;\
+    cd nimble; nim -d:release c -r src/nimble -y install;\
+    ln -s `pwd`/nimble /bin/nimble
+#  end if
+ENV PATH="~/.nimble/bin:$${PATH}"
 #end proc
 #
 #proc alpine*(version: string,
 #             labels: openarray[(string, string)] = {:}): string =
 #  result = ""
-# # These are known to have supported ssl library versions
-#  if version >= "0.17.0":
-FROM alpine:3.5
-#  else:
-FROM alpine:3.4
-#  end if
+FROM nimlang/nim:$version-alpine-slim
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apk add --no-cache g++ curl tar xz
-RUN mkdir -p /nim; \
-    curl -sL "http://nim-lang.org/download/nim-${version}.tar.xz" \
-    |tar xJ --strip-components=1 -C /nim; \
-    cd /nim; sh build.sh; \
-    ln -s `pwd`/bin/nim /bin/nim
+RUN apk add --no-cache git mercurial
+#  if version >= "0.16.0":
+RUN cd nim; nim c koch; ./koch tools; \
+    ln -s `pwd`/bin/nimble /bin/nimble; \
+    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest
+#  else:
+RUN git clone https://github.com/nim-lang/nimble.git;\
+    cd nimble; nim -d:release c -r src/nimble -y install;\
+    ln -s `pwd`/nimble /bin/nimble
+#  end if
+ENV PATH="~/.nimble/bin:$${PATH}"
 #end proc
