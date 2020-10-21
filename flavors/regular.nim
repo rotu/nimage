75da42950880cd1c6c6d6a8fe6aa4c0748efdd8b
Constantine Molchanov <moigagoo@live.com>
2020-10-21 20:59:57 +0400

    Add symlinks to testament.

--- flavors/regular.nim
+++ flavors/regular.nim
@@ -10,7 +10,8 @@ RUN apt-get update; apt-get install -y git mercurial
 #  if version >= "0.16.0":
 RUN cd nim; nim c koch; ./koch tools; \
     ln -s `pwd`/bin/nimble /bin/nimble; \
-    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest
+    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest \
+    ln -s `pwd`/bin/testament /bin/testament
 #  else:
 RUN git clone https://github.com/nim-lang/nimble.git;\
     cd nimble; nim -d:release c -r src/nimble -y install;\
@@ -30,7 +31,8 @@ RUN apk add --no-cache git mercurial
 #  if version >= "0.16.0":
 RUN cd nim; nim c koch; ./koch tools; \
     ln -s `pwd`/bin/nimble /bin/nimble; \
-    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest
+    ln -s `pwd`/bin/nimsuggest /bin/nimsuggest \
+    ln -s `pwd`/bin/testament /bin/testament
 #  else:
 RUN git clone https://github.com/nim-lang/nimble.git;\
     cd nimble; nim -d:release c -r src/nimble -y install;\
